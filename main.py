917e771608aff3eba7391a350346a574b9a817b7
saros1249 <saros1249@gmail.com>
2022-07-29 15:30:46 +0800

    Начал программу.

--- main.py
+++ main.py
@@ -113,8 +113,11 @@ if __name__ == '__main__':
     print('В магазине доступно:')
     print(shop.get_items())
     while True:
-        amount, product, where_from = input('Введите количество и название товара, откуда его нужно забрать через запятую: ')
+        amount = input('Введите количество товара, которое нужно переместить:')
+        product = input('Введите название товара, который нужно переместить:')
+        where_from = input('Введите место, откуда его нужно забрать товар:')
         print(f'Курьер забирает {amount} {product} из {where_from}')
+
 
         if where_from == 'склад':
             var_d = store.get_items()
@@ -122,8 +125,8 @@ if __name__ == '__main__':
             var_d = shop.get_items()
         else:
             print(f'{where_from} - такого места не существует.')
-
-        if {product: amount} in var_d and amount <= var_d[product]:
-            print(f'Нужное количество есть на {where_from}')
-        else:
-            print(f'Не хватает на {where_from}, попробуйте заказать меньше')
+        print(var_d)
+        # if {product: amount} in var_d and amount <= var_d[product]:
+        #     print(f'Нужное количество есть на {where_from}')
+        # else:
+        #     print(f'Не хватает на {where_from}, попробуйте заказать меньше')
