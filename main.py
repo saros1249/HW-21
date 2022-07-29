0d2294cf10413540593b331357cb183b1c761a97
saros1249 <saros1249@gmail.com>
2022-07-29 15:20:31 +0800

    Начал программу.

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ class Store(Storage):
         self._items = {'печенье': 5, 'ёлка': 9, 'ёжик': 2, 'вода': 10, 'пиво': 16, 'хлеб': 25}
         self._capacity: int = capacity
 
-
+    def get_items(self):
+        return self._get_items
 
     @property
     def get_free_space(self):
@@ -47,8 +48,11 @@ class Store(Storage):
 class Shop(Storage):
 
     def __init__(self, capacity=20):
-        self._items = {}
+        self._items = {'ёлка': 1, 'ёжик': 1, 'вода': 5, 'пиво': 6, 'хлеб': 2}
         self._capacity: int = capacity
+
+    def get_items(self):
+        return self._get_items
 
     @property
     def get_free_space(self):
@@ -85,26 +89,41 @@ class Shop(Storage):
             print('Нет запрошенного количества')
 
 
-class Request:
-
-    def __init__(self):
-        pass
-
-    def way(self, amount, product, where_from, to):
-        return where_from, to, amount, product
+# class Request:
+#
+#     def __init__(self):
+#         pass
+#
+#     def way(self, amount, product, where_from, to):
+#         return where_from, to, amount, product
 
 if __name__ == '__main__':
+    # print('Здравствуйте. Вас приветствует симулятор логистики. Давайте познакомимся. Как Вас зовут?')
+    # name = input('Введите ваше имя: ')
+    # print(f'Здрвствуйте, {name}! Начнём?')
+    # var = input(f'Введите "y", если хотите продолжить, или "n", если хотите выйти')
+    # if var == 'n':
+    #     quit()
+    # else:
+    store = Store()
+    shop = Shop()
+    # request = Request()
+    print('На складе доступно:')
+    print(store.get_items())
+    print('В магазине доступно:')
+    print(shop.get_items())
+    while True:
+        amount, product, where_from = input('Введите количество и название товара, откуда его нужно забрать через запятую: ')
+        print(f'Курьер забирает {amount} {product} из {where_from}')
 
-    print('Здравствуйте. Вас приветствует симулятор логистики. Давайте познакомимся. Как Вас зовут?')
-    name = input('Введите ваше имя: ')
-    print(f'Здрвствуйте, {name}! Начнём?')
-    var = input(f'Введите "y", если хотите продолжить, или "n", если хотите выйти')
-    if var == 'n':
-        quit()
-    else:
-        store = Store()
-        shop = Shop()
-        request = Request()
-        print('')
+        if where_from == 'склад':
+            var_d = store.get_items()
+        elif where_from == 'магазин':
+            var_d = shop.get_items()
+        else:
+            print(f'{where_from} - такого места не существует.')
 
-
+        if {product: amount} in var_d and amount <= var_d[product]:
+            print(f'Нужное количество есть на {where_from}')
+        else:
+            print(f'Не хватает на {where_from}, попробуйте заказать меньше')
